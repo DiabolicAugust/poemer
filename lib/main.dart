f2905e17c2ad6410607331bbc4678e0646c80203
HlibBrodskyi <h.brodskyi@newlinetechnologies.net>
2023-04-17 21:11:09 +0300

    - Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:poemer/presentation/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'network/bindings/directories_binding.dart';
+import 'network/controllers/directories_controller.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Get.putAsync(() async => DirectoriesController());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(1920, 1080),
+      builder:(context,_)=>  GetMaterialApp(
+        initialBinding: DirectoriesBinding(),
+        debugShowCheckedModeBanner: false,
+        home:  HomePage(),
+      ),
+    );
+  }
+}
